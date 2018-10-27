00ca6ca57eb62e0dc3d0d913cfc02108136d151c
CYD <374766849@qq.com>
2018-10-27 13:07:24 +0800

    lab08

--- ProtectWM/offline.js
+++ ProtectWM/offline.js
@@ -1,9 +1,23 @@
 ﻿{
-	"version": 1540615028,
+	"version": 1540616794,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
-		"offlineClient.js"
+		"offlineClient.js",
+		"images/semicircle-sheet0.png",
+		"images/watermelon-sheet0.png",
+		"images/fork-sheet0.png",
+		"images/begin-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/water-sheet0.png",
+		"images/restart-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
 	]
 }
